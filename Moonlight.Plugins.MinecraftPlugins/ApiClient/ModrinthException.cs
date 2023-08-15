42b5e3e9a90e9e8e95c3c61f21b3ca9e33948bfb
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-08-15 21:57:11 +0200

    Added minecraft plugins plugin

---
+++ Moonlight.Plugins.MinecraftPlugins/ApiClient/ModrinthException.cs
@@ -0,0 +1,19 @@
+﻿namespace Moonlight.Plugins.MinecraftPlugins.ApiClient;
+
+public class ModrinthException : Exception
+{
+    public int StatusCode { get; set; }
+    
+    public ModrinthException()
+    {
+    }
+
+    public ModrinthException(string message, int statusCode) : base(message)
+    {
+        StatusCode = statusCode;
+    }
+
+    public ModrinthException(string message, Exception inner) : base(message, inner)
+    {
+    }
+}
---
+++ Moonlight.Plugins.MinecraftPlugins/MinecraftPluginsPlugin.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Moonlight.App.Helpers;
+using Moonlight.App.Plugin;
+using Moonlight.Plugins.MinecraftPlugins.ApiClient;
+using Moonlight.Plugins.MinecraftPlugins.Services;
+using Moonlight.Plugins.MinecraftPlugins.UI;
+
+namespace Moonlight.Plugins.MinecraftPlugins;
+
+public class MinecraftPluginsPlugin : MoonlightPlugin
+{
+    public MinecraftPluginsPlugin()
+    {
+        Name = "MinecraftPlugins";
+        Version = "15.08.2023";
+        Author = "Moonlight Panel";
+
+        OnBuildServerPage += context =>
+        {
+            if (context.ImageTags.Any(x => x == "minecraft-plugins"))
+            {
+                context.Tabs.Add(new()
+                {
+                    Name = "Plugins",
+                    Icon = "bxs-customize",
+                    Route = "/plugins",
+                    Component = ComponentHelper.FromType(typeof(PluginPage))
+                });
+            }
+            
+            return Task.CompletedTask;
+        };
+
+        OnBuildServices += collection =>
+        {
+            collection.AddScoped<MinecraftPluginService>();
+            collection.AddSingleton<ModrinthApiHelper>();
+            
+            return Task.CompletedTask;
+        };
+    }
+}
