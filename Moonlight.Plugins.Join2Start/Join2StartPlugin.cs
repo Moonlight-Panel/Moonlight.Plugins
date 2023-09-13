265e81afd2e4bc433712dcc8418d96e76e1fd84d
Marcel Baumgartner <marcel.kbkm@gmail.com>
2023-09-13 18:21:12 +0200

    Added join2start plugin

---
+++ Moonlight.Plugins.Join2Start/Join2StartPlugin.cs
@@ -0,0 +1,23 @@
+﻿using Moonlight.App.Plugin;
+
+namespace Moonlight.Plugins.Join2Start;
+
+public class Join2StartPlugin : MoonlightPlugin
+{
+    public Join2StartPlugin()
+    {
+        Name = "Join2Start";
+        Version = "13.09.2023";
+        Author = "Moonlight Panel";
+
+        OnBuildServerPage += context =>
+        {
+            context.Settings.Add(new()
+            {
+                Name = "Join2Start"
+            });
+            
+            return Task.CompletedTask;
+        };
+    }
+}
